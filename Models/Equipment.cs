a6cc55d2ee12f00c0e0b2c7467c62ca66aea63ba
Diana Martseniuk <diana.martsenyuk@gmail.com>
2026-03-27 18:25:03 +0100

    Add status to equipment class

---
+++ Models/Equipment.cs
@@ -0,0 +1,17 @@
+namespace UniEquipmentRental.Models;
+
+public class Equipment
+{
+    public int Id { get; }
+    private static int LastId;
+    public string Name { get; private set; }
+    public EquipmentStatus Status { get; protected set; }
+
+    public Equipment (string name)
+    {
+        LastId++;
+        Id = LastId;
+        Name = name;
+        Status = EquipmentStatus.Available;
+    }
+}
